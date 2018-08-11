67d67e10cfe6647542e4656f52dc540db9dd7549
chenbi <chenbi@users.noreply.github.com>
2018-08-10 23:52:10 -0400

    Update cgminer.rb

--- cgminer.rb
+++ cgminer.rb
@@ -3,9 +3,9 @@ require 'formula'
 class Cgminer < Formula
   homepage 'https://github.com/ckolivas/cgminer'
   head 'https://github.com/ckolivas/cgminer.git', :branch => 'master'
-  url 'https://github.com/ckolivas/cgminer/archive/v4.10.0.zip'
+  url 'https://github.com/ckolivas/cgminer/archive/v3.7.2.zip'
   sha256 '7815778d77a3c340fe2de6d7f76b16ebc5a3aa8f04ef4d4051ea9b910e7606c5'
-  version '4.10.0'
+  version '3.7.2'
 
   depends_on 'autoconf' => :build
   depends_on 'automake' => :build
