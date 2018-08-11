5613e13c6566d2d4d37c58daac94a4bb8919e868
chenbi <chenbi@users.noreply.github.com>
2018-08-11 00:07:16 -0400

    Update cgminer.rb

--- cgminer.rb
+++ cgminer.rb
@@ -3,38 +3,34 @@ require 'formula'
 class Cgminer < Formula
   homepage 'https://github.com/ckolivas/cgminer'
   head 'https://github.com/ckolivas/cgminer.git', :branch => 'master'
-  url 'https://github.com/ckolivas/cgminer/archive/v3.7.2.zip'
-  sha256 'a9fc205ce2bc0ad52b83890dfac41d12053e792dbe3a09a0876f41dba8d616a4'
-  version '3.7.2'
+  url 'https://github.com/ckolivas/cgminer/archive/v3.7.2.tar.gz'
+  sha1 '806527947d40024be6201d66ef80722f21092977'
 
   depends_on 'autoconf' => :build
   depends_on 'automake' => :build
   depends_on 'libtool' => :build
   depends_on 'pkg-config' => :build
   depends_on 'coreutils' => :build
-  depends_on 'libusb' => :build
   depends_on 'curl'
+  depends_on 'jansson'
+  depends_on 'libusb'
 
   def install
+    inreplace "autogen.sh", "libtoolize", "glibtoolize"
+    inreplace "autogen.sh", "readlink", "greadlink"
     system "autoreconf -fvi"
     system "./autogen.sh", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "PKG_CONFIG_PATH=#{HOMEBREW_PREFIX}/opt/curl/lib/pkgconfig:#{HOMEBREW_PREFIX}/opt/jansson/lib/pkgconfig:#{HOMEBREW_PREFIX}/opt/libusb/lib/pkgconfig",
+                          "--enable-scrypt",
                           "--enable-bflsc",
                           "--enable-bitforce",
                           "--enable-icarus",
                           "--enable-modminer",
+                          "--enable-ztex",
                           "--enable-avalon",
                           "--disable-adl",
-                          "--enable-opencl",
-                          "--enable-bitfury",
-                          "--enable-hashfast",
-                          "--enable-klondike",
-                          "--enable-drillbit",
-                          "--enable-avalon2",
-                          "--enable-blockerupter",
-                          "--enable-cointerra",
-                          "--enable-hashratio"
+                          "--enable-opencl"
     system "make", "install"
   end
 
